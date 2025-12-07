2c3f38ab6f10bd9e3ee4f856e9c94ecd49fe14d1
nq4n <moayed9919@gmail.com>
2025-12-07 19:32:17 +0400

    fix subabase clinet

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,12 +1,9 @@
-import { createBrowserClient } from '@supabase/ssr'
+import { createClient as createBrowserClient } from '@supabase/supabase-js'
 import { getSupabaseConfig } from '../env'
 import { Database } from './database.types'
 
 export function createClient() {
   const { supabaseUrl, supabaseAnonKey } = getSupabaseConfig()
 
-  return createBrowserClient<Database>(
-    supabaseUrl,
-    supabaseAnonKey
-  )
+  return createBrowserClient<Database>(supabaseUrl, supabaseAnonKey)
 }
