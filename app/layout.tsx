ede78974e57ecae1d693332a9d6565a71f40da36
nq4n <moayed9919@gmail.com>
2025-12-10 21:25:30 +0400

    fix build issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import { Navbar } from "@/components/layout/navbar";
 import AuthProvider from "@/components/auth-provider";
 import { Toaster } from "@/components/ui/sonner";
-
+import { Suspense } from "react";
 export const metadata: Metadata = {
   title: "EduPack",
   description: "Create SCORM packages with ease.",
@@ -13,11 +13,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
+        <Suspense fallback={<div>Loading...</div>}>
         <AuthProvider>
           <Navbar />
           <main>{children}</main>
           <Toaster />
         </AuthProvider>
+        </Suspense>
       </body>
     </html>
   );
